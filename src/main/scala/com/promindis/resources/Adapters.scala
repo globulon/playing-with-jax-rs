9f3b3aa72f68337ec10cb278ecc2b11e0564dc26
Marc-Daniel Ortega <globulon@gmail.com>
2012-02-17 17:54:19 +0100

    hacke Martin work and know how Scala Lists are serializable

--- src/main/scala/com/promindis/resources/Adapters.scala
+++ src/main/scala/com/promindis/resources/Adapters.scala
@@ -3,9 +3,7 @@ package com.promindis.resources
 import javax.xml.bind.annotation.adapters.XmlAdapter
 import scala.collection._
 import java.util.{List => JList, ArrayList => AList}
-import javax.xml.bind.annotation.XmlRootElement._
-import javax.xml.bind.annotation.{XmlElementRef, XmlAccessType, XmlAccessorType, XmlRootElement}
-import java.util.Arrays.ArrayList
+import javax.xml.bind.annotation._
 
 /**
  * Date: 17/02/12
@@ -22,19 +20,22 @@ object Adapters {
     override  def embed(list: JList[Book]) = BookContainer(list)
   }
 
-  @XmlRootElement(name = "items")
-  @XmlAccessorType(XmlAccessType.FIELD)
-  case class BookContainer(@XmlElementRef(name = "items") items: JList[Book]) extends Container[Book] {
+  @XmlRootElement
+  @XmlAccessorType(XmlAccessType.PROPERTY)
+  @XmlType(propOrder = Array("book"))
+  case class BookContainer(items: JList[Book]) extends Container[Book] {
     def this() = this(new AList[Book]())
+
+    @XmlElement
+    def getBook = items
   }
 
   trait ListToContainer[T , A <: Container[T]] extends XmlAdapter[A, List[T]] {
-
-    import JavaConverters._
+  import JavaConverters._
 
     def embed(list: JList[T]): A
 
-    def marshal(seq: List[T]) = embed(seq.asJava)
+    def marshal(seq: List[T]) = if (!seq.isEmpty) embed(seq.asJava) else embed(new AList[T])
 
     def unmarshal(container: A) = container.items.asScala.toList
   }
